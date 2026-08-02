e07d977e1019fe2b5dba2da30572704a7aa174b5
jhopan <jhosuaarmandoputrapanjaitan@gmail.com>
2026-08-03 00:27:21 +0800

    fix: normalize quota before Lua tonumber

---
+++ tests/test-lua-tonumber.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+set -eu
+FILE=$(CDPATH= cd -- "$(dirname -- "$0")/.." && pwd)/luci/model/cbi/bandwidth_control.lua
+! grep -q 'tonumber((.*:gsub' "$FILE"
+grep -q 'local raw_quota=' "$FILE"
+printf 'ok\n'
