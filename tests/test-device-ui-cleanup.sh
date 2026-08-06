7f7503dcd373ee3da0a11786123bb2e5e1286b7b
jhopan <jhosuaarmandoputrapanjaitan@gmail.com>
2026-08-06 13:22:26 +0800

    fix: hide replacement column outside edit mode

--- tests/test-device-ui-cleanup.sh
+++ tests/test-device-ui-cleanup.sh
@@ -8,4 +8,6 @@ CSS="$ROOT/luci/view/bandwidth_control/styles.htm"
 ! grep -q 'Each saved device is locked' "$CBI"
 grep -q 'Current device' "$CBI"
 grep -q 'bc-mac-edit-select' "$CSS"
+grep -q ':has(.bc-mac-edit-active)' "$CSS"
+grep -q 'Click Edit only' "$CBI"
 printf 'ok\n'
