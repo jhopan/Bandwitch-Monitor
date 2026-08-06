8cb8d76cbbe0fb586330244f8d5fa9efa08ba63a
jhopan <jhosuaarmandoputrapanjaitan@gmail.com>
2026-08-06 23:07:38 +0800

    fix: strip CRLF from all view/lua files; add sed in build-ipk.sh to prevent future CRLF in IPK

--- build-ipk.sh
+++ build-ipk.sh
@@ -14,6 +14,7 @@ mkdir -p "$STAGE/data/usr/lib/lua/luci"
 cp -a "$ROOT/luci/controller" "$STAGE/data/usr/lib/lua/luci/"
 cp -a "$ROOT/luci/model" "$STAGE/data/usr/lib/lua/luci/"
 cp -a "$ROOT/luci/view" "$STAGE/data/usr/lib/lua/luci/"
+find "$STAGE/data/usr/lib/lua/luci/view" -name "*.htm" -exec sed -i 's/\r//' {} \;
 chmod 0755 "$STAGE/control/postinst" "$STAGE/data/etc/init.d/bandwidth-control" "$STAGE/data/usr/libexec/bandwidth-control/check"
 printf '2.0\n' > "$STAGE/debian-binary"
 tar -C "$STAGE/control" -czf "$STAGE/control.tar.gz" .
