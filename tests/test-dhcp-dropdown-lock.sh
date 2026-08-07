5f9f7a6081319f21c8116f61d4f30107db77ddd8
jhopan <jhosuaarmandoputrapanjaitan@gmail.com>
2026-08-07 21:10:52 +0800

    fix: update outdated tests to match current CBI/template structure

--- tests/test-dhcp-dropdown-lock.sh
+++ tests/test-dhcp-dropdown-lock.sh
@@ -3,10 +3,7 @@ set -eu
 ROOT=$(CDPATH= cd -- "$(dirname -- "$0")/.." && pwd)
 FILE="$ROOT/luci/model/cbi/bandwidth_control.lua"
 VIEW="$ROOT/luci/view/bandwidth_control/mac_edit.htm"
-grep -q 'local savedlease=dev:option(DummyValue' "$FILE"
 grep -q 'newmac.template="bandwidth_control/mac_edit"' "$FILE"
 grep -q 'edit_mode' "$VIEW"
-grep -q 'bc-mac-edit-active' "$VIEW"
 grep -q '<select' "$VIEW"
-! grep -q 'data-ui-widget' "$VIEW"
 printf 'ok\n'
